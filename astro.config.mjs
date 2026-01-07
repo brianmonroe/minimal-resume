1f22d8e9d839430de9b4b4920d276b3305a78bef
brianmonroe <brianfostermonroe@gmail.com>
2026-01-06 21:23:18 -0500

    add astro config for github

--- astro.config.mjs
+++ astro.config.mjs
@@ -3,5 +3,7 @@ import tailwind from "@astrojs/tailwind";
 
 export default defineConfig({
   integrations: [tailwind()],
-  output: "static"
+  output: "static",
+  site: "https://brianmonroe.github.io",
+  base: "/minimal-resume/",
 });
