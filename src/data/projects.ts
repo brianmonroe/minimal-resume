63d7fb883e058187aafc14767a57425a3439d621
brianmonroe <brianfostermonroe@gmail.com>
2026-07-28 16:27:40 -0400

    add photo gallery images

--- src/data/projects.ts
+++ src/data/projects.ts
@@ -3,6 +3,7 @@ export type ProjectItem = {
   link?: string;
   gallery?: boolean;
   icon: string;
+  image: string;
   title: string;
   description: string;
 };
@@ -10,9 +11,10 @@ export type ProjectItem = {
 export const projects: ProjectItem[] = [
   {
     type: "Ecommerce",
-    title: "TotallyPromotional.com",
+    title: "Totally Promotional",
     description: "Collaborated with a small in-house and international development team to implement marketing and user-experience features across a large-scale e-commerce site built on the Znode platform.",
     icon: "ecommerce",
+    image: "/images/projects/tp.webp",
     link: "https://totallypromotional.com",
   },
   {
@@ -20,6 +22,7 @@ export const projects: ProjectItem[] = [
     title: "Celina Glass Company",
     description: "A clean, responsive contractor portfolio built with Astro + Bootstrap and SEO-friendly structure. This project included a domain and hosting transfer.",
     icon: "html",
+    image: "/images/projects/celinaglass.webp",
     link: "http://celinaglass.com/",
   },
   {
@@ -27,20 +30,23 @@ export const projects: ProjectItem[] = [
     title: "Angie King - State Representative",
     description: "Personal biography website for State Representative Angie King. Built with Astro + Tailwind.",
     icon: "html",
-    link: "https://dev.kingforohio.com",
+    image: "/images/projects/angieking.webp",
+    link: "https://www.kingforohio.com",
   },
   {
     type: "Small Organization Website",
     title: "Celina Moose Lodge #1473",
     description: "General information and event calendar website for the Celina Moose. Featured a calender that pulls event data from Google Sheets.",
     icon: "html",
-    link: "https://www.kingforohio.com",
+    image: "/images/projects/celinamoose.webp",
+    link: "https://www.celinamoose.com",
   },
   {
     type: "Interactive HTML/JS",
     title: "Draft Bingo Generator",
     description: "A single-file HTML/JS application that generates a custom Bingo card and ability to randomize and print a custom number of cards.",
     icon: "html",
+    image: "/images/projects/moosebingo.webp",
     link: "https://celinamoose.com/hidden/nfl-draft-bingo.html",
   },
   {
@@ -48,6 +54,7 @@ export const projects: ProjectItem[] = [
     title: "Photo Portfolio",
     description: "A collection of photography samples captured with Nikon DSLR equipment and processed in Adobe Lightroom.",
     icon: "photography",
+    image: "/images/projects/photogallery.webp",
     gallery: true,
   },
 ];
